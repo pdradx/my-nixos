51a19ef3deceb3c91b2330bd92ab1a0f74698028
pdradx <pdradx@gmail.com>
2026-02-03 10:21:23 +0600

    Enable flakes support

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
