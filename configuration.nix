e78de8bc9205286e034de4d2ebcf3eed99de7dee
pdradx <pdradx@gmail.com>
2026-02-13 10:53:51 +0600

    Disable some preinstalled GNOME packages

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,24 @@
     '';
   };
 
+  environment.gnome.excludePackages = (with pkgs; [
+    atomix # puzzle game
+    #cheese # webcam tool
+    epiphany # web browser
+    #evince # document viewer
+    geary # email reader
+    #gedit # text editor
+    #gnome-characters
+    #gnome-music
+    #gnome-photos
+    #gnome-terminal
+    #gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    #totem # video player
+  ]);
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -124,6 +142,11 @@
     ripgrep
     yazi
     bat
+    telegram-desktop
+    
+    
+
+
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
