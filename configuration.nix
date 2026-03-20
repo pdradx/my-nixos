187ea1c2676c15d9b780b6fc67ece3fa04a05eaa
Pavel <pdradx@gmail.com>
2026-03-20 14:37:58 +0600

    Add openvpn plugin for NetworkManager

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = [ pkgs.networkmanager-openvpn ];
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Omsk";
